3d0584c985cead6d75b4e00524aeebe27e94904c
RickChen1220 <zx12201993@gmail.com>
2023-03-21 01:06:52 +0800

    UseParams on account page and add dynamic classes

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,10 +1,11 @@
 import { useContext } from "react";
-import { Link, Navigate } from "react-router-dom";
+import { Link, Navigate, useParams } from "react-router-dom";
 import { UserContext } from "../UserContext";
 
 export default function AccountPage() {
   const { ready, user } = useContext(UserContext);
-
+  const { subpage } = useParams();
+  console.log(subpage);
   if (!ready) {
     return "Lodaing...";
   }
@@ -13,21 +14,22 @@ export default function AccountPage() {
     return <Navigate to={"/login"} />;
   }
 
+  function linkClasses(type = null) {
+    let classes = "py-2 px-6";
+    if (type === subpage) {
+      classes += " bg-primary text-white rounded-xl";
+    }
+    return classes;
+  }
+
   return (
     <div>
       <nav className="w-full flex justify-center mt-8 gap-2">
-        <Link
-          className="py-2 px-6 bg-primary text-white rounded-xl"
-          to={"/account"}
-        >
+        <Link className={linkClasses("profile")} to={"/account"}>
           My account
         </Link>
-        <Link className="py-2 px-6" to={"/account/bookings"}>
-          My bookings
-        </Link>
-        <Link className="py-2 px-6" to={"/account/places"}>
-          My room
-        </Link>
+        <Link className={linkClasses("bookings")} to={"/account/bookings"}>My bookings</Link>
+        <Link className={linkClasses("places")} to={"/account/places"}>My room</Link>
       </nav>
     </div>
   );
