9ec1b3a8b95ac12e1cee0196cb6a19fc793bb94a
RickChen1220 <zx12201993@gmail.com>
2023-04-07 00:35:03 +0800

    Add IndexPage

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import {Link} from "react-router-dom";
+
 export default function IndexPage() {
   const [places, setPlaces] = useState("");
   useEffect(() => {
@@ -11,7 +13,7 @@ export default function IndexPage() {
     <div className="mt-8 gap-x-6 gap-y-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
       {places.length > 0 &&
         places.map((place) => (
-          <div>
+          <Link to={"/place/"+place._id}>
             <div className="mb-2 bg-slate-500 rounded-2xl flex">
               {place.photos?.[0] && (
                 <img
@@ -26,7 +28,7 @@ export default function IndexPage() {
             <div className="mt-1">
               <span className="font-bold">${place.price}</span> per night
             </div>
-          </div>
+          </Link>
         ))}
     </div>
   );
