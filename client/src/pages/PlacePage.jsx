a16ced7f987898967461610e9ce83567242cbc33
RickChen1220 <zx12201993@gmail.com>
2023-04-07 16:57:27 +0800

    Display photo on PlacePage

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -1,8 +1,60 @@
-import { useParams } from "react-router-dom"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function PlacePage() {
-    const {id} = useParams();
-    return(
-        <div>place page:{id}</div>
-    )
+  const { id } = useParams();
+  const [place, setPlace] = useState(null);
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    axios.get(`/places/${id}`).then((res) => {
+      setPlace(res.data);
+    });
+  }, [id]);
+  if (!place) return "";
+  return (
+    <div className="mt-4 bg-slate-100 -mx-8 px-8 py-8">
+      <h1 className="text-3xl">{place.title}</h1>
+      <a
+        className="my-2 block font-semibold underline"
+        target="_blank"
+        href={"http://maps.google.com/?q=" + place.address}
+      >
+        {place.address}
+      </a>
+      <div className="relative">
+        <div className="grid gap-2 grid-cols-[2fr_1fr]">
+          <div>
+            {place.photos?.[0] && (
+              <div>
+                <img
+                  className="aspect-square object-cover"
+                  src={"http://localhost:4000/uploads/" + place.photos[0]}
+                />
+              </div>
+            )}
+          </div>
+          <div className="grid">
+            {place.photos?.[1] && (
+              <img
+                className="aspect-square object-cover"
+                src={"http://localhost:4000/uploads/" + place.photos[1]}
+              />
+            )}
+            <div className="overflow-hidden">
+              {place.photos?.[2] && (
+                <img
+                  className="aspect-square object-cover relative top-2"
+                  src={"http://localhost:4000/uploads/" + place.photos[2]}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      <button>Show more photos</button>
+    </div>
+  );
 }
