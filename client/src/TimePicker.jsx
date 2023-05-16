4f1c106bbeda320095b39df120c4c1aa5ede9f0e
RickChen1220 <zx12201993@gmail.com>
2023-05-17 01:10:02 +0800

    Fix:Use Luxon in TimePicker

--- client/src/TimePicker.jsx
+++ client/src/TimePicker.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { DateTime } from "luxon";
 
 export default function TimePicker({ onTimeSelect, selectedDate }) {
   const { id } = useParams();
@@ -31,38 +32,41 @@ export default function TimePicker({ onTimeSelect, selectedDate }) {
 
   if (!place) return "";
 
-  const now = new Date();
-  const isToday = selectedDate.toDateString() === now.toDateString();
+  const now = DateTime.now();
+  const isToday = selectedDate.toDateString() === now.toJSDate().toDateString();
 
   // Create an array of time slots with 30-minute intervals
   const timeSlots = [];
-  let startTime = new Date(selectedDate);
-  startTime.setHours(place.openTime);
-  startTime.setMinutes(0);
+  let startTime = DateTime.fromJSDate(selectedDate);
+  startTime = startTime.set({
+    hour: place.openTime,
+    minute: 0,
+  });
 
-  let endTime = new Date(selectedDate);
-  endTime.setHours(place.closeTime);
-  endTime.setMinutes(0);
+  let endTime = DateTime.fromJSDate(selectedDate);
+  endTime = endTime.set({
+    hour: place.closeTime,
+    minute: 0,
+  });
   // Subtract one hour from closing time to allow for the last booking to end at the closing time
-  endTime.setMinutes(endTime.getMinutes() - 30);
+  endTime = endTime.minus({ minutes: 30 });
+
 
   if (isToday) {
     // If the selected date is today, set the start time to the current time or the opening time of the place
-    const currentHour = now.getHours();
-    const currentMinute = now.getMinutes();
+    const currentHour = now.hour;
+    const currentMinute = now.minute;
     if (currentHour < place.openTime) {
-      startTime.setHours(place.openTime);
-      startTime.setMinutes(0);
+      startTime = startTime.set({ hour: place.openTime, minute: 0 });
     } else if (currentHour >= place.closeTime) {
       return "";
     } else {
       if (currentMinute < 30) {
         // Round up to the nearest 30-minute interval
-        startTime.setMinutes(30);
+        startTime = startTime.plus({ minutes: 30 - currentMinute });
       } else {
-        startTime.setHours(currentHour);
-        startTime.setMinutes(0);
-        startTime.setMinutes(startTime.getMinutes() + 30);
+        startTime = startTime.set({ hour: currentHour, minute: 0 });
+        startTime = startTime.plus({ minutes: 30 });
       }
     }
     // If the current time is after the closing time of the place, don't show any time slots
@@ -72,16 +76,15 @@ export default function TimePicker({ onTimeSelect, selectedDate }) {
   }
 
   while (startTime < endTime) {
+    // If the selected date is today, skip time slots that are before the current time
     const timeSlot = new Date(startTime);
-    if (isToday && timeSlot <= now) {
-      // If the selected date is today, skip time slots that are before the current time
-      startTime.setMinutes(startTime.getMinutes() + 30);
+    if (isToday && timeSlot <= now.toJSDate()) {
+      startTime = startTime.plus({ minutes: 30 });
       continue;
     }
     timeSlots.push(timeSlot);
-    startTime.setMinutes(startTime.getMinutes() + 30);
+    startTime = startTime.plus({ minutes: 30 });
   }
-
   // Create a label for the time range
   const label = `Opening time: from ${place.openTime} to ${place.closeTime}`;
 
@@ -112,7 +115,7 @@ export default function TimePicker({ onTimeSelect, selectedDate }) {
                 }`}
                 onClick={() =>
                   handleTimeChange(
-                    timeSlot.toLocaleTimeString([], {
+                    DateTime.fromJSDate(timeSlot).toLocaleString({
                       hour: "2-digit",
                       minute: "2-digit",
                     })
@@ -120,10 +123,14 @@ export default function TimePicker({ onTimeSelect, selectedDate }) {
                 }
               >
                 <span hidden className="text-xs text-center leading-none">
-                  {timeSlot.toLocaleDateString()}
+                  {DateTime.fromJSDate(timeSlot).toLocaleString({
+                    weekday: "short",
+                    month: "short",
+                    day: "numeric",
+                  })}
                 </span>
                 <span className="text-md text-center leading-none">
-                  {timeSlot.toLocaleTimeString([], {
+                  {DateTime.fromJSDate(timeSlot).toLocaleString({
                     hour: "2-digit",
                     minute: "2-digit",
                   })}
