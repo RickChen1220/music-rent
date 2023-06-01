9c2cd1035e3da78f996467ed7eba7a0b856cdfef
RickChen1220 <zx12201993@gmail.com>
2023-06-02 01:21:13 +0800

    Fix:Make checkout time slot dynamic in TimePicker.

--- client/src/TimePicker.jsx
+++ client/src/TimePicker.jsx
@@ -14,6 +14,20 @@ export default function TimePicker({
   const [selectedTime, setSelectedTime] = useState("");
   const [checkoutTime, setCheckoutTime] = useState("");
 
+  const now = DateTime.now();
+  const isToday = selectedDate.toDateString() === now.toJSDate().toDateString();
+
+   // Calculate the minimum time for time slot generation based on the selected time
+   let minimumTime = DateTime.fromJSDate(selectedDate).set({
+    hour: place.openTime,
+    minute: 0,
+  });
+
+  // If it's today and a selected time exists, set the minimum time to be after the selected time
+  if(isToday && selectedTime) {
+    minimumTime = DateTime.fromFormat(selectedTime, "HH:mm");
+  }
+
   useEffect(() => {
     if (!id) {
       return;
@@ -29,11 +43,12 @@ export default function TimePicker({
 
   if (!place) return "";
 
-  const now = DateTime.now();
-  const isToday = selectedDate.toDateString() === now.toJSDate().toDateString();
 
-  // Create an array of time slots with 30-minute intervals
-  const timeSlots = [];
+
+
+  // Filter the time slots to include only the slots after the minimum time
+  const timeSlots= [];
+  let timeSlotsFiltered = timeSlots.filter(timeSlot => timeSlot.timeSlotObj >= minimumTime);
   let startTime = DateTime.fromJSDate(selectedDate);
   startTime = startTime.set({
     hour: place.openTime,
@@ -46,7 +61,7 @@ export default function TimePicker({
     minute: 0,
   });
   // Subtract one hour from closing time to allow for the last booking to end at the closing time
-  endTime = endTime.minus({ minutes: 30 });
+  /* endTime = endTime.minus({ minutes: 30 }); */
 
   if (isToday) {
     // If the selected date is today, set the start time to the current time or the opening time of the place
@@ -81,7 +96,7 @@ export default function TimePicker({
     const timeSlotLabel = timeSlotObj.toFormat("EEEE, LLLL d");
     const timeSlotTime = timeSlotObj.toFormat("HH:mm");
 
-    timeSlots.push({
+    timeSlotsFiltered.push({
       timeSlotObj,
       timeSlotLabel,
       timeSlotTime,
@@ -110,6 +125,18 @@ export default function TimePicker({
     onCheckoutTimeChange(selectedTime);
   }
 
+  // Calculate the checkout start time (one hour after the selected time)
+  const checkoutStartTime = DateTime.fromFormat(selectedTime, "HH:mm").plus({
+    hours: 1,
+  });
+
+  // Filter the time slots for the checkout time range
+  const checkoutTimeSlots = timeSlotsFiltered.filter(
+    (timeSlot) =>
+      timeSlot.timeSlotObj >= checkoutStartTime &&
+      timeSlot.timeSlotObj <= endTime
+  );
+
   return (
     <div>
       <div className="rounded-2xl p-2 shadow">
@@ -120,7 +147,7 @@ export default function TimePicker({
           aria-labelledby="time-range-label"
           className="flex flex-wrap justify-center"
         >
-          {timeSlots.map((timeSlot) => {
+          {timeSlotsFiltered.map((timeSlot) => {
             const { timeSlotObj, timeSlotLabel, timeSlotTime } = timeSlot;
 
             const timeSlotBoxClasses = `m-2 p-2 rounded-2xl ${
@@ -151,15 +178,7 @@ export default function TimePicker({
             Select a time slot for checkout:
           </div>
           <div className="flex flex-wrap justify-center">
-            {timeSlots
-              .filter(
-                (timeSlot) =>
-                  timeSlot.timeSlotObj >
-                  DateTime.fromFormat(selectedTime, "HH:mm").plus({
-                    minutes: 30,
-                  })
-              )
-              .map((timeSlot) => {
+            {checkoutTimeSlots.map((timeSlot) => {
                 const { timeSlotObj, timeSlotTime } = timeSlot;
 
                 const checkoutTimeBoxClasses = `m-2 p-2 rounded-2xl ${
